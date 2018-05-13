ef85e0463d9b1718d13869027fcf192dbb965231
Appinventiv <sumit.patel93@gmail.com>
2018-05-13 12:59:19 +0530

    chaneges for index.js for heroku error

--- routes/index.js
+++ routes/index.js
@@ -6,17 +6,7 @@ var router = express.Router();
 
 
 router.get('/', (req, res, next) => {
-	request('', function (error, response, body) {		
-			if (typeof web3 !== 'undefined'){ 
-    web3 = new Web3(web3.currentProvider);
-}
-else{
-web3 =  new Web3(new Web3.providers.HttpProvider("http://104.196.220.141:8545"));
-var bal = web3.eth.getBalance("0xa723b9a63ae67b78b94412cd574937210152ddef")
-console.log(bal.toNumber());
-}
-
-	});	
+res.send("hi")	
 });
 
 module.exports = router;
