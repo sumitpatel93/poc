4c58236c3b6cd8c73a1855ca0c1c43d8be5c93de
Appinventiv <sumit.patel93@gmail.com>
2018-05-22 01:47:01 +0530

    finally token transfer

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,11 @@
 var Web3 = require('web3');
 var express = require('express');
-
 var router = express.Router();
 var fs    = require("fs");
 var EthereumTx = require('ethereumjs-tx');
 var crypto = require('crypto');
 var secp256k1 = require('secp256k1');
 var keccak =  require("keccak");
-
-
 
 
 router.get('/', (req, res, next) => {
@@ -22,24 +19,25 @@ if ( typeof web3 !== 'undefined' && typeof web3 !== undefined) {
 
    web3 =  new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/tPNyqnWI32IepQh2lrMf"));
  
-   var count = web3.eth.getTransactionCount("0xD8d9e3fE93315b8ce838E39c4696438A8d8E66F3");
-var abiArray = JSON.parse(fs.readFileSync('./public/mycoin.json', 'utf-8'));
-var contractAddress = "0x318a624cD1cc9B9830fE2B353816Fa2014f3D72d";
-var contract = web3.eth.contract(abiArray).at(contractAddress);
+   var count = web3.eth.getTransactionCount("0xBed8b4981f2432498e501dea46a767025E7fAE9a");
+   var abiArray = JSON.parse(fs.readFileSync('./public/mycoin.json', 'utf-8'));
+   var contractAddress = "0x53576813682009a4D35495e18f039067618B3e27";
+   var contract = web3.eth.contract(abiArray).at(contractAddress);
    var number =  web3.eth.blockNumber;
    var rawTransaction = {
-    "from": "0xbb7f52b085a2b4e567c0e12cc37d01572c5ff764",
-    "nonce":"0x002"  ,
-    "gasPrice": "0x9184e72a0",
-    "gasLimit": "0x7459",
-    "to": "0xd98077dAe57Fb61902AbA993ac32Cfb1213639B3",
-    "value": "0x012",
-    "data": contract.transfer.getData("0xd98077dAe57Fb61902AbA993ac32Cfb1213639B3", 100, {from: "0xbb7f52b085a2b4e567c0e12cc37d01572c5ff764"}),
-    "chainId": "0x04"
+    "from": "0xBed8b4981f2432498e501dea46a767025E7fAE9a",
+    "nonce":web3.toHex(count)  ,  //keep track of trx happening in the _from block.
+    "gasPrice": web3.toHex(10000000000),
+    "gasLimit": web3.toHex(500000),
+    "to": "0x53576813682009a4D35495e18f039067618B3e27",
+    "value": web3.toHex(0), //sends the amount of ether _to address
+    "data": contract.transfer.getData("0xDED54CC5f578Aa39c1083dbe16D68a28C776cA02", 100, {from: "0xBed8b4981f2432498e501dea46a767025E7fAE9a"}),
+    "chainId": "0x04" //rinkeby id 
 };
 
-var privKey = new Buffer('ea54c4ee7d53f48d6e7120e07eb21d77d11383e2be4634b164537e9227ed71f1', 'hex');
+var privKey = new Buffer('a8b5b0c767d2914b18bf3a7dff89e95ad856e80153b247c385e8ba97ce037081', 'hex');
 console.log('else');
+console.log("transaction count is" , count);
 var tx = new EthereumTx(rawTransaction);
 
 tx.sign(privKey);
